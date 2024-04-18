4466e5e142f7a6c6d8ac290d4afbe49ceb566b92
Daniel Stahl <danstahl1138@gmail.com>
2024-04-18 10:49:20 -0500

    update hull white, make enum work

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 use serde_derive::Deserialize;
+use serde_enum_str::Deserialize_enum_str;
 use serde_json::json;
 
-use lambda_http::{lambda, Body, IntoResponse, Request, RequestExt, Response};
-use lambda_runtime::{error::HandlerError, Context};
+use hull_white::HullWhite;
+use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
+
 use rand::distributions::StandardNormal;
 use rand::{thread_rng, Rng};
 use rayon::prelude::*;
 use std::collections::HashMap;
-use std::error::Error;
 use std::f64;
 
-fn build_response(code: u16, body: &str) -> impl IntoResponse {
-    Response::builder()
+fn build_response(code: u16, body: &str) -> Result<Response<Body>, Error> {
+    Ok(Response::builder()
         .status(code)
         .header("Access-Control-Allow-Origin", "*")
         .header("Access-Control-Allow-Credentials", "true")
-        .body::<Body>(body.into())
-        .unwrap()
+        .body::<Body>(body.into())?)
 }
 fn construct_error(e_message: &str) -> String {
     json!({ "err": e_message }).to_string()
@@ -46,25 +46,38 @@ fn generate_vasicek(curr_rate: f64, a: f64, b: f64, sig: f64, t: f64) -> impl Fn
 
 const NUM_SIMS: usize = 500;
 
+#[derive(Deserialize_enum_str, Debug)]
+#[serde(rename_all = "lowercase")]
+enum Asset {
+    BOND,
+    EDF,
+    BONDCALL,
+    BONDPUT,
+    CAPLET,
+    SWAP,
+    SWAPTION,
+    AMERICANSWAPTION,
+}
+
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct BondParameters {
-    t: f64, //in days
+struct BaseParameters {
+    t: f64,
     r0: f64,
     a: f64,
     b: f64,
     sigma: f64,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct BondParameters {
     maturity: f64,
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct EDFParameters {
-    t: f64, //in days
-    r0: f64,
-    a: f64,
-    b: f64,
-    sigma: f64,
     maturity: f64,
     tenor: f64,
 }
@@ -72,11 +85,6 @@ struct EDFParameters {
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct BondOptionParameters {
-    t: f64, //in days
-    r0: f64,
-    a: f64,
-    b: f64,
-    sigma: f64,
     maturity: f64,
     underlying_maturity: f64,
     strike: f64,
@@ -85,11 +93,6 @@ struct BondOptionParameters {
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct CapletParameters {
-    t: f64, //in days
-    r0: f64,
-    a: f64,
-    b: f64,
-    sigma: f64,
     maturity: f64,
     tenor: f64,
     strike: f64,
@@ -98,11 +101,6 @@ struct CapletParameters {
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct SwapParameters {
-    t: f64, //in days
-    r0: f64,
-    a: f64,
-    b: f64,
-    sigma: f64,
     maturity: f64,
     tenor: f64,
     swap_rate: f64,
@@ -111,14 +109,9 @@ struct SwapParameters {
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct SwaptionParameters {
-    t: f64, //in days
-    r0: f64,
-    a: f64,
-    b: f64,
-    sigma: f64,
     maturity: f64,
     tenor: f64,
-    swap_tenor: f64,
+    num_swap_payments: usize,
     swap_rate: f64,
 }
 
@@ -150,14 +143,16 @@ fn combine_and_bin(min: f64, max: f64, elements: &[f64]) -> HashMap<String, usiz
     bin(min, max, num_bins, elements)
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    lambda!(market_faas_wrapper);
-    Ok(())
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let func = service_fn(func);
+    run(func).await
 }
-fn market_faas_wrapper(event: Request, _ctx: Context) -> Result<impl IntoResponse, HandlerError> {
+
+async fn func(event: Request) -> Result<Response<Body>, Error> {
     match market_faas(event) {
-        Ok(res) => Ok(build_response(200, &json!(res).to_string())),
-        Err(e) => Ok(build_response(400, &construct_error(&e.to_string()))),
+        Ok(res) => Ok(build_response(200, &json!(res).to_string())?),
+        Err(e) => Ok(build_response(400, &construct_error(&e.to_string()))?),
     }
 }
 
@@ -196,257 +191,130 @@ where
 fn transform_days_to_year(t: f64) -> f64 {
     t / 365.0
 }
-fn bond(event: Request) -> Result<HashMap<String, usize>, Box<dyn Error>> {
-    let BondParameters {
-        t,
-        r0,
-        a,
-        b,
-        maturity,
-        sigma,
-    } = serde_json::from_reader(event.body().as_ref())?;
+
+fn market_faas(event: Request) -> Result<HashMap<String, usize>, Box<dyn std::error::Error>> {
+    let asset: Asset = event
+        .path_parameters()
+        .first("asset")
+        .unwrap_or("bond")
+        .parse::<Asset>()?;
+
+    let BaseParameters { t, a, r0, b, sigma } = serde_json::from_reader(event.body().as_ref())?;
     let t = transform_days_to_year(t);
     let yield_fn = yield_curve(r0, a, b, sigma);
     let forward_fn = forward_curve(r0, a, b, sigma);
+    let hull_white = HullWhite::init(a, sigma, &yield_fn, &forward_fn);
     let simulation = generate_vasicek(r0, a, b, sigma, t);
-    let func_to_sim = |random_number: f64| {
-        let r_t = simulation(random_number);
-        hull_white::bond_price_t(r_t, a, sigma, t, maturity, &yield_fn, &forward_fn)
-    };
-    Ok(mc(NUM_SIMS, &func_to_sim))
-}
-fn market_faas(event: Request) -> Result<HashMap<String, usize>, Box<dyn Error>> {
-    let path_parameters = event.path_parameters();
-    let asset = path_parameters.get("asset").unwrap_or("bond");
-    let histogram = match asset {
-        "bond" => bond(event)?,
-        "edf" => {
-            let EDFParameters {
-                t,
-                r0,
-                a,
-                b,
-                maturity,
-                sigma,
-                tenor,
-            } = serde_json::from_reader(event.body().as_ref())?;
-            let t = transform_days_to_year(t);
-            let yield_fn = yield_curve(r0, a, b, sigma);
-            let forward_fn = forward_curve(r0, a, b, sigma);
-            let simulation = generate_vasicek(r0, a, b, sigma, t);
+    match asset {
+        Asset::BOND => {
+            let BondParameters { maturity } = serde_json::from_reader(event.body().as_ref())?;
             let func_to_sim = |random_number: f64| {
                 let r_t = simulation(random_number);
-                hull_white::euro_dollar_future_t(
-                    r_t,
-                    a,
-                    sigma,
-                    t,
-                    maturity,
-                    tenor,
-                    &yield_fn,
-                    &forward_fn,
-                )
+                hull_white.bond_price_t(r_t, t, maturity)
             };
-            mc(NUM_SIMS, &func_to_sim)
+            Ok(mc(NUM_SIMS, &func_to_sim))
         }
-        "bondcall" => {
+        Asset::EDF => {
+            let EDFParameters { maturity, tenor } = serde_json::from_reader(event.body().as_ref())?;
+            let func_to_sim = |random_number: f64| {
+                let r_t = simulation(random_number);
+                hull_white.euro_dollar_future_t(r_t, t, maturity, tenor)
+            };
+            Ok(mc(NUM_SIMS, &func_to_sim))
+        }
+        Asset::BONDCALL => {
             let BondOptionParameters {
-                t,
-                r0,
-                a,
-                b,
                 maturity,
-                sigma,
                 underlying_maturity,
                 strike,
             } = serde_json::from_reader(event.body().as_ref())?;
-            let t = transform_days_to_year(t);
-            let yield_fn = yield_curve(r0, a, b, sigma);
-            let forward_fn = forward_curve(r0, a, b, sigma);
-            let simulation = generate_vasicek(r0, a, b, sigma, t);
             let func_to_sim = |random_number: f64| {
                 let r_t = simulation(random_number);
-                hull_white::bond_call_t(
-                    r_t,
-                    a,
-                    sigma,
-                    t,
-                    maturity,
-                    underlying_maturity,
-                    strike,
-                    &yield_fn,
-                    &forward_fn,
-                )
+                hull_white.bond_call_t(r_t, t, maturity, underlying_maturity, strike)
             };
-            mc(NUM_SIMS, &func_to_sim)
+            Ok(mc(NUM_SIMS, &func_to_sim))
         }
-        "bondput" => {
+        Asset::BONDPUT => {
             let BondOptionParameters {
-                t,
-                r0,
-                a,
-                b,
                 maturity,
-                sigma,
                 underlying_maturity,
                 strike,
             } = serde_json::from_reader(event.body().as_ref())?;
-            let t = transform_days_to_year(t);
-            let yield_fn = yield_curve(r0, a, b, sigma);
-            let forward_fn = forward_curve(r0, a, b, sigma);
-            let simulation = generate_vasicek(r0, a, b, sigma, t);
             let func_to_sim = |random_number: f64| {
                 let r_t = simulation(random_number);
-                hull_white::bond_put_t(
-                    r_t,
-                    a,
-                    sigma,
-                    t,
-                    maturity,
-                    underlying_maturity,
-                    strike,
-                    &yield_fn,
-                    &forward_fn,
-                )
+                hull_white.bond_put_t(r_t, t, maturity, underlying_maturity, strike)
             };
-            mc(NUM_SIMS, &func_to_sim)
+            Ok(mc(NUM_SIMS, &func_to_sim))
         }
-        "caplet" => {
+        Asset::CAPLET => {
             let CapletParameters {
-                t,
-                r0,
-                a,
-                b,
                 maturity,
-                sigma,
                 strike,
                 tenor,
             } = serde_json::from_reader(event.body().as_ref())?;
-            let t = transform_days_to_year(t);
-            let yield_fn = yield_curve(r0, a, b, sigma);
-            let forward_fn = forward_curve(r0, a, b, sigma);
-            let simulation = generate_vasicek(r0, a, b, sigma, t);
             let func_to_sim = |random_number: f64| {
                 let r_t = simulation(random_number);
-                hull_white::caplet_t(
-                    r_t,
-                    a,
-                    sigma,
-                    t,
-                    maturity,
-                    tenor,
-                    strike,
-                    &yield_fn,
-                    &forward_fn,
-                )
+                hull_white.caplet_t(r_t, t, maturity, tenor, strike)
             };
-            mc(NUM_SIMS, &func_to_sim)
+            Ok(mc(NUM_SIMS, &func_to_sim))
         }
-        "swap" => {
+        Asset::SWAP => {
             let SwapParameters {
-                t,
-                r0,
-                a,
-                b,
                 maturity,
-                sigma,
                 swap_rate,
                 tenor,
             } = serde_json::from_reader(event.body().as_ref())?;
-            let t = transform_days_to_year(t);
-            let yield_fn = yield_curve(r0, a, b, sigma);
-            let forward_fn = forward_curve(r0, a, b, sigma);
-            let simulation = generate_vasicek(r0, a, b, sigma, t);
             let func_to_sim = |random_number: f64| {
                 let r_t = simulation(random_number);
-                hull_white::swap_price_t(
-                    r_t,
-                    a,
-                    sigma,
-                    t,
-                    maturity,
-                    tenor,
-                    swap_rate,
-                    &yield_fn,
-                    &forward_fn,
-                )
+                hull_white.swap_price_t(r_t, t, maturity, tenor, swap_rate)
             };
-            mc(NUM_SIMS, &func_to_sim)
+            Ok(mc(NUM_SIMS, &func_to_sim))
         }
-        "swaption" => {
+        Asset::SWAPTION => {
             let SwaptionParameters {
-                t,
-                r0,
-                a,
-                b,
                 maturity,
-                sigma,
                 swap_rate,
+                num_swap_payments,
                 tenor,
-                swap_tenor,
             } = serde_json::from_reader(event.body().as_ref())?;
-            let t = transform_days_to_year(t);
-            let yield_fn = yield_curve(r0, a, b, sigma);
-            let forward_fn = forward_curve(r0, a, b, sigma);
-            let simulation = generate_vasicek(r0, a, b, sigma, t);
             let func_to_sim = |random_number: f64| {
                 let r_t = simulation(random_number);
-                hull_white::european_payer_swaption_t(
-                    r_t,
-                    a,
-                    sigma,
-                    t,
-                    swap_tenor,
-                    maturity,
-                    tenor,
-                    swap_rate,
-                    &yield_fn,
-                    &forward_fn,
-                )
+                hull_white
+                    .european_payer_swaption_t(
+                        r_t,
+                        t,
+                        maturity,
+                        num_swap_payments,
+                        tenor,
+                        swap_rate,
+                    )
+                    .unwrap()
             };
-            mc(NUM_SIMS, &func_to_sim)
+            Ok(mc(NUM_SIMS, &func_to_sim))
         }
-        "americanswaption" => {
+        Asset::AMERICANSWAPTION => {
             let SwaptionParameters {
-                t,
-                r0,
-                a,
-                b,
                 maturity,
-                sigma,
                 swap_rate,
+                num_swap_payments,
                 tenor,
-                swap_tenor,
             } = serde_json::from_reader(event.body().as_ref())?;
-            let t = transform_days_to_year(t);
-            let yield_fn = yield_curve(r0, a, b, sigma);
-            let forward_fn = forward_curve(r0, a, b, sigma);
-            let simulation = generate_vasicek(r0, a, b, sigma, t);
             let num_tree = 100;
             let func_to_sim = |random_number: f64| {
                 let r_t = simulation(random_number);
-                hull_white::american_payer_swaption_t(
+                hull_white.american_payer_swaption_t(
                     r_t,
-                    a,
-                    sigma,
                     t,
-                    swap_tenor,
                     maturity,
+                    num_swap_payments,
                     tenor,
                     swap_rate,
                     num_tree,
-                    &yield_fn,
-                    &forward_fn,
                 )
             };
-            mc(NUM_SIMS, &func_to_sim)
+            Ok(mc(NUM_SIMS, &func_to_sim))
         }
-        _ => {
-            //I wish the compiler was smarter than this...we know it wont get here
-            bond(event)?
-        }
-    };
-    Ok(histogram)
+    }
 }
 
 #[cfg(test)]
@@ -491,7 +359,19 @@ mod tests {
         assert_eq!(average_result < 0.052, true);
         assert_eq!(average_result > 0.048, true);
     }
-    #[test]
+    #[tokio::test]
+    async fn test_bond_simulation() {
+        let input = include_str!("gateway_request.json");
+        let request = lambda_http::request::from_str(&input).expect("failed to create request");
+        let body = func(request).await.unwrap(); //.body();
+        match &body.body() {
+            Body::Text(text) => println!("{}", text),
+            _ => println!("Not text"),
+        }
+        assert!(!body.body().is_empty());
+    }
+
+    /*#[test]
     fn bond_simulation() {
         let r = 0.04;
         let a = 0.3;
@@ -527,16 +407,7 @@ mod tests {
         let forward_fn = forward_curve(r, a, b, sig);
         let func_to_sim = |random_number: f64| {
             let r_t = simulation(random_number);
-            hull_white::euro_dollar_future_t(
-                r_t,
-                a,
-                sig,
-                t,
-                maturity,
-                delta,
-                &yield_fn,
-                &forward_fn,
-            )
+            hull_white.euro_dollar_future_t(r_t, a, sig, t, maturity, delta, &yield_fn, &forward_fn)
         };
         let n = 500;
         let results = mc_results(n, &func_to_sim);
@@ -612,7 +483,7 @@ mod tests {
         println!("this is average: {}", average_result);
         assert_eq!(average_result < 0.006, true);
         assert_eq!(average_result > 0.005, true);
-    }
+    }*/
     #[test]
     fn min_v_test() {
         let v = vec![4.0, 2.0, 5.0];
